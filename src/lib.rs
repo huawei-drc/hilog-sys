de5c680ab57abb3196dd92af64847c5b0c4b69a8
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-04-22 17:09:39 +0200

    Add optional `log` Level conversion

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,28 @@
 //! - Does not contain any `printf` style format specifiers (like `%s`, `%d`) OR
 //! - `fmt` is `"${public}s\0"` and the actual string is passed as the following parameter.
 //!
+//! ## Crate Features
+//!
+//! * `log`: When the log feature is enabled, a `From<log::Level>` implementation is added
+//!          to easily convert from `log`s log level to HiLogs log level.
+//!
 //! [hilog native guidelines]: https://gitee.com/openharmony/docs/blob/master/en/application-dev/dfx/hilog-guidelines.md
 
 #[allow(non_snake_case)]
 mod ffi;
 
 pub use ffi::*;
+
+#[cfg(feature = "log")]
+impl From<log::Level> for LogLevel {
+    fn from(level: log::Level) -> Self {
+        // Note: log:::Level::Trace has no corresponding HiLog level.
+        match level {
+            log::Level::Error => LogLevel::LOG_ERROR,
+            log::Level::Warn => LogLevel::LOG_WARN,
+            log::Level::Info => LogLevel::LOG_INFO,
+            log::Level::Debug => LogLevel::LOG_DEBUG,
+            log::Level::Trace => LogLevel::LOG_DEBUG,
+        }
+    }
+}
